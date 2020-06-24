d17aaf31fc585fbfde9668098130d110940d0fca
wangdingfeng <wangdingfeng@live.com>
2020-06-24 14:32:34 +0800

    完善代码生成器

--- spark-admin/spark-admin-biz/src/main/java/com/spark/platform/adminbiz/service/gen/impl/GenServiceImpl.java
+++ spark-admin/spark-admin-biz/src/main/java/com/spark/platform/adminbiz/service/gen/impl/GenServiceImpl.java
@@ -2,12 +2,17 @@ package com.spark.platform.adminbiz.service.gen.impl;
 
 import cn.hutool.core.util.ZipUtil;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.FileOutConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import com.spark.platform.adminapi.entity.gen.TableColumnInfo;
@@ -25,6 +30,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -99,6 +105,35 @@ public class GenServiceImpl implements GenService {
         gc.setBaseColumnList(true);
         gc.setBaseResultMap(true);
         gc.setEnableCache(false);
+
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+                        + "/" + tableInfo.getEntityName() + "Dao" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setController("/templates/controller.java");
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
---
+++ spark-admin/spark-admin-biz/src/main/resources/templates/controller.java.ftl
@@ -0,0 +1,78 @@
+package ${package.Controller};
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import ${package.Service}.${table.serviceName};
+import ${package.Entity}.${entity};
+import com.spark.platform.common.base.support.ApiResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+<#if restControllerStyle>
+import org.springframework.web.bind.annotation.RestController;
+<#else>
+import org.springframework.stereotype.Controller;
+</#if>
+<#if superControllerClassPackage??>
+import ${superControllerClassPackage};
+</#if>
+
+/**
+ * <p>
+ * ${table.comment!} api访问层
+ * </p>
+ *
+ * @author ${author}
+ * @since ${date}
+ */
+<#if restControllerStyle>
+@RestController
+<#else>
+@Controller
+</#if>
+@RequestMapping("<#if package.ModuleName??>/${package.ModuleName}</#if>/<#if controllerMappingHyphenStyle??>${controllerMappingHyphen}<#else>${table.entityPath}</#if>")
+<#if kotlin>
+class ${table.controllerName}<#if superControllerClass??> : ${superControllerClass}()</#if>
+<#else>
+@Api(tags = "${table.comment!}")
+@AllArgsConstructor
+<#if superControllerClass??>
+public class ${table.controllerName} extends ${superControllerClass} {
+<#else>
+public class ${table.controllerName} {
+</#if>
+
+  private final ${table.serviceName} ${table.serviceName ? uncap_first};
+
+  @GetMapping("/page")
+  @ApiOperation(value = "${table.comment!}列表")
+  public ApiResponse page(${entity} ${entity ? uncap_first}, Page page){
+   return success(${table.serviceName ? uncap_first}.page(page,Wrappers.query(${entity ? uncap_first})));
+  }
+
+  @PostMapping
+  @ApiOperation(value = "保存${table.comment!}信息")
+  public ApiResponse save(@RequestBody ${entity} ${entity ? uncap_first}){
+   return success(${table.serviceName ? uncap_first}.save(${entity ? uncap_first}));
+  }
+
+  @PutMapping
+  @ApiOperation(value = "更新${table.comment!}信息")
+  public ApiResponse save(@RequestBody ${entity} ${entity ? uncap_first}){
+   return success(${table.serviceName ? uncap_first}.updateById(${entity ? uncap_first}));
+  }
+
+  @DeleteMapping("/{id}")
+  @ApiOperation(value = "删除${table.comment!}")
+  public ApiResponse save(@PathVariable Long id){
+   return success(${table.serviceName ? uncap_first}.removeById(id));
+  }
+
+  @GetMapping("/{id}")
+  @ApiOperation(value = "根据id获取${table.comment!}信息")
+  public ApiResponse getRoleByUserId(@PathVariable Long id) {
+   return success(${table.serviceName ? uncap_first}.getById(id));
+  }
+
+}
+</#if>
